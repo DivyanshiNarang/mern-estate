6674115a2d3a3e8186284cba9a83367c5338990c
Divyanshi <divyanshi.narang16@gmail.com>
2023-12-22 02:51:16 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    //to check whether connection is established
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
